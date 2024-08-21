3f875595641452e60aeb0b9c32b62723c0358654
Igor Serrao <igorserrao93@gmail.com>
2024-08-21 14:39:17 -0300

    first commit

---
+++ src/componentes/Ordenacao/index.jsx
@@ -0,0 +1,14 @@
+import './styles.css'
+
+export default function Ordenacao () {
+    return (
+            <ul className="container-ordenacao">
+                <li >
+                    <a href="" className='container-ordenacao__ativo'>Recentes</a>
+                </li>
+                <li>
+                    <a href="" className='container-ordenacao__cinza'>Recentes</a>
+                </li>
+            </ul>
+    )
+}
